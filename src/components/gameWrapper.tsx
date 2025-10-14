e90418e403db476ddf3aaa496324f1536581a2ee
alissa-u <alice7ui@gmail.com>
2025-10-14 20:33:57 +0300

    Add header

--- src/components/gameWrapper.tsx
+++ src/components/gameWrapper.tsx
@@ -1,9 +1,11 @@
 import Board from "./board";
+import Header from "./header/header";
 import Keyboard from "./keyboard/keyboard";
 
 const GameWrapper = () => {
   return (
     <div className="wrapper container mx-auto flex flex-col max-w-md">
+      <Header />
       <Board />
       <Keyboard />
     </div>
---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="flex flex-row max-w-lg py-2 px-3 border-b dark:border-neutral-700">
+      <div className="flex-auto text-center">
+        <div className="uppercase font-extrabold text-2xl small:text-xl tiny:text-base tracking-wider">
+          WORDLE
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
