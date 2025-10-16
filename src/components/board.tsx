1ccfa4b69da80c18af866fd0901aa1b0fd10a291
Arnosha96 <arnosha96@mail.ru>
2025-10-16 19:10:45 +0300

    Cells height fix

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -3,7 +3,7 @@ import CellsLine from "./cellsLine";
 const Board = () => {
   return (
     <main className="flex flex-auto justify-center items-center">
-      <div className="flex flex-col gap-1 p-3 box-border fix-h fix-small-iphones-grid w-full max-w-[360px] max-h-[480px]">
+      <div className="flex flex-col gap-1 p-3 box-border h-full min-h-60 w-full max-w-[360px] max-h-[480px]">
         <CellsLine />
         <CellsLine />
         <CellsLine />
--- src/components/cellsLine.tsx
+++ src/components/cellsLine.tsx
@@ -2,7 +2,7 @@ import Cell from "./cell";
 
 const CellsLine = () => {
   return (
-    <div className="flex gap-1 w-full fix-tile-width">
+    <div className="flex h-full max-h-20 gap-1 w-full fix-tile-width">
       <Cell letter={"W"} />
       <Cell letter={"O"} />
       <Cell letter={"R"} />
