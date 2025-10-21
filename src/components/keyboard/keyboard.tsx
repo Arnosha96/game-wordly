c486f805bafe23aa77c9a482e976710524379b67
Arnosha96 <arnosha96@mail.ru>
2025-10-21 19:33:09 +0300

    Small refactoring

--- src/components/keyboard/keyboard.tsx
+++ src/components/keyboard/keyboard.tsx
@@ -5,46 +5,20 @@ type KeyboardProps = {
 };
 
 const Keyboard = ({ onKeyboardInput }: KeyboardProps) => {
+  const firstLine = ['й','ц','у','к','е','н','г','ш','щ','з','х','ъ']
+  const middleLine = ['ф','ы','в','а','п','р','о','л','д','ж','э']
+  const lastLine = ['я','ч','с','м','и','т','ь','б','ю']
   return (
     <div className="flex flex-col container w-full max-w-lg pb-2 md:pb-5 px-2 mx-auto">
       <div className="flex">
-        <Key letter="й" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="ц" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="у" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="к" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="е" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="н" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="г" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="ш" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="щ" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="з" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="х" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="ъ" handleClick={(letter) => onKeyboardInput(letter)}></Key>
+        {firstLine.map((symbol) => <Key letter={symbol} handleClick={(letter) => onKeyboardInput(letter)}></Key>)}
       </div>
       <div className="flex">
-        <Key letter="ф" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="ы" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="в" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="а" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="п" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="р" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="о" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="л" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="д" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="ж" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="э" handleClick={(letter) => onKeyboardInput(letter)}></Key>
+        {middleLine.map((symbol) => <Key letter={symbol} handleClick={(letter) => onKeyboardInput(letter)}></Key>)}
       </div>
       <div className="flex">
         <Key letter="<=" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="я" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="ч" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="с" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="м" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="и" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="т" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="ь" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="б" handleClick={(letter) => onKeyboardInput(letter)}></Key>
-        <Key letter="ю" handleClick={(letter) => onKeyboardInput(letter)}></Key>
+        {lastLine.map((symbol) => <Key letter={symbol} handleClick={(letter) => onKeyboardInput(letter)}></Key>)}
         <Key letter="ввод" handleClick={(letter) => onKeyboardInput(letter)}></Key>
       </div>
     </div>
